3bf2d0e7780e0dfe32fb94ef2ed0ce518ac2ba51
singham3 <40882089+singham3@users.noreply.github.com>
2026-02-26 19:13:19 +0530

    remove unused package

--- src/components/upload.jsx
+++ src/components/upload.jsx
@@ -3,7 +3,6 @@ import { useEffect, useState } from "react";
 import {
   getAllUploads,
   getUploadState,
-  clearUploadState,
 } from "../utils/uploadStorage";
 import { cancelUpload } from "../utils/uploadMultipart";
 
